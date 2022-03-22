ee4b6cc34bafbd6e0f94351666a4168a52306260
thibautmicetic <90472927+thibautmicetic@users.noreply.github.com>
2022-03-22 11:17:46 +0100

    Add files via upload

---
+++ Mes projets/Projet 3/Brouillon.py
@@ -0,0 +1,27 @@
+# Créé par tmicetic, le 22/03/2022 en Python 3.7
+
+"""labyrinthe = [
+  [[2, False, True, True, False],[2, False, True, False, True],[2, False, False, False, False],[2, False, False, False, False]],
+  [[2, False, False, False, False],[2, False, False, False, False],[2, False, False, False, False],[2, False, False, False, False]],
+  [[2, False, False, False, False],[2, False, False, False, False],[2, False, False, False, False],[2, False, False, False, False]],
+  [[2, False, False, False, False],[2, False, False, False, False],[2, False, False, False, False],[2, False, False, False, False]],
+]
+
+def position_entree(liste):
+    for i in range(len(liste)):
+        for y in range()
+  pass"""
+
+l = [[True, True, False], [True, False, False]]
+for i in range(len(l)):
+    sousliste = l[i]
+    print(sousliste)
+
+    #for elements in l[i]:
+    #    print(elements)
+
+
+
+"""fromages = ['Cheddar', 'Edam', 'Gouda']
+for fromage in fromages:
+    print(fromage)"""
