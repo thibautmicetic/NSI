e477a253a403cf3875cc93d3ebb78a4bf80ce475
thibautmicetic <90472927+thibautmicetic@users.noreply.github.com>
2022-03-22 11:53:47 +0100

    Add files via upload

---
+++ Mes projets/Projet 3/entree_sortie.py
@@ -0,0 +1,10 @@
+# Créé par tmicetic, le 22/03/2022 en Python 3.7
+import random
+1, 2, 3, 4 = ligne, colonne, derniere_ligne, derniere_colonne
+
+
+def definir_entree_sorite(liste):
+    cote = random.randint(1, 4)
+    if cote == 1:
+        for i in range(len(cote)):
+            pass
