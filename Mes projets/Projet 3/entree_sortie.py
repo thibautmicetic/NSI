074cf3cc4061964762b826d26780c4b4d5dfc84d
thibautmicetic <90472927+thibautmicetic@users.noreply.github.com>
2022-03-22 19:47:10 +0100

    Update entree_sortie.py

--- Mes projets/Projet 3/entree_sortie.py
+++ Mes projets/Projet 3/entree_sortie.py
@@ -1,20 +1,29 @@
 # Créé par tmicetic, le 22/03/2022 en Python 3.7
 import random
 
+labyrinthe = [
+  [[2, False, True, True, False],[2, False, True, False, True],[2, False, True, False, True],[1, False, False, False, True]],
+  [[2, True, False, True, False],[2, False, True, False, False],[2, False, True, True, True],[0, False, False, True, True]],
+  [[2, True, True, False, False],[2, False, False, True, True],[2, True, False, True, False],[2, True, False, True, False]],
+  [[2, False, False, True, False],[2, True, True, False, False],[2, True, False, False, True],[2, False, True, False, False]],
+]
+
+for i in range(len(labyrinthe)):
+    sousliste = labyrinthe[i]
+    print(sousliste)
+
 def definir_entree_sorite():
-    cote_entree = random.randint(1, 4)
-    cote_sortie = 0
-    if cote_entree == 1:
-        cote_sortie = 3
-    elif cote_entree == 2:
-        cote_sortie = 4
-    entree = random.randint(1, 15)
-    sortie = random.randint(1, 15)
-    if cote_entree == 1:
-        pass
-    elif cote_entree == 2:
-        pass
-    elif cote_entree == 3:
-        pass
-    elif cote_entree == 4:
-        pass
+    """ Fonction qui définit l'entrée et la sortie du labyrinthe """
+    premier_cote = random.randint(1, 2)
+    if premier_cote == 1:
+        deuxieme_cote = 3
+    elif premier_cote == 2:
+        deuxieme_cote = 4
+    for i in range(len(labyrinthe)):
+        for elements in labyrinthe[i]:
+            labyrinthe[i][0] = 2
+    carre_entree = random.randint(1, 15)
+    carre_sortie = random.randint(1, 15)
+    carre_actuel = labyrinthe[carre_entree]
+    carre_actuel[0] = 1
+    carre_actuel[1] = True
